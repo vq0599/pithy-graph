970f970f4f2021fc2a12d855eee30e7fe2b2f157
dehaohuang <hdh1@meitu.com>
2022-11-15 22:26:44 +0800

    元素箭头移动

--- src/utils/element-events-bind.ts
+++ src/utils/element-events-bind.ts
@@ -1,5 +1,6 @@
 import { canvasStore } from '@/stores/canvas';
 import { preziStore } from '@/stores/prezi';
+import { IElement } from '@/structs';
 
 let copyFromElement = 0;
 let copyFromSlide = 0;
@@ -8,19 +9,44 @@ export const bindKeyboardEvents = ({ code, metaKey }: KeyboardEvent) => {
   // 处于输入状态
   if (canvasStore.editing) return;
 
-  if (preziStore.currentElement) {
-    if (['Delete', 'Backspace'].includes(code)) {
-      preziStore.deleteElement();
-    } else if (code === 'KeyC' && metaKey) {
-      copyFromElement = preziStore.currentElementId;
-      copyFromSlide = preziStore.currentSlideId;
+  // 不需要选中也可以触发的事件
+  if (!preziStore.currentElement) {
+    if (code === 'KeyV' && metaKey) {
+      if (copyFromElement && copyFromSlide) {
+        preziStore.copyElement(copyFromElement, copyFromSlide);
+      }
     }
+    return;
   }
 
-  // 不需要选中也可以触发的事件
-  if (code === 'KeyV' && metaKey) {
-    if (copyFromElement && copyFromSlide) {
-      preziStore.copyElement(copyFromElement, copyFromSlide);
+  // 以下为必须要先选中才能触发的事件
+  if (['Delete', 'Backspace'].includes(code)) {
+    preziStore.deleteElement();
+  } else if (code === 'KeyC' && metaKey) {
+    copyFromElement = preziStore.currentElementId;
+    copyFromSlide = preziStore.currentSlideId;
+  } else if (
+    ['ArrowDown', 'ArrowUp', 'ArrowLeft', 'ArrowRight'].includes(code)
+  ) {
+    const { x, y } = preziStore.currentElement;
+    const options: Partial<Pick<IElement, 'x' | 'y'>> = {};
+    const unit = 1 / canvasStore.scale;
+    switch (code) {
+      case 'ArrowUp':
+        options.y = y - unit;
+        break;
+      case 'ArrowDown':
+        options.y = y + unit;
+        break;
+      case 'ArrowLeft':
+        options.x = x - unit;
+        break;
+      case 'ArrowRight':
+        options.x = x + unit;
+        break;
+      default:
+        break;
     }
+    preziStore.updateElement(options);
   }
 };
